dcbca0fdebafc7900283b5004e4ed605b4be811a
0lowe <joshua.colin.lowe@gmail.com>
2023-05-27 05:54:06 +0200

    changed colors and added icon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontSize: {
-				sm: ['14px', '20px'],
-				base: ['16px', '24px'],
-				lg: ['20px', '28px'],
-				xl: ['24px', '32px'],
-			},
-			fontFamily: {
-				sans: ['DM Sans', 'sans-serif'],
-				heading: ['Poppins', 'sans-serif']
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: ["14px", "20px"],
+        base: ["16px", "24px"],
+        lg: ["20px", "28px"],
+        xl: ["24px", "32px"],
+      },
+      colors: {
+        primary: "#20537F",
+        secondary: "#2C74B3",
+        white: "#FCFEFF",
+        black: "#1E2022",
+      },
+      fontFamily: {
+        sans: ["DM Sans", "sans-serif"],
+        heading: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
